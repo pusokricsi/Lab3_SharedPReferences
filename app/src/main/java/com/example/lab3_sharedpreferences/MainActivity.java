042bd5bd168bd6dc322e54d1af31367b3f683b59
pusokricsi <Daewoo12345>
2019-10-12 15:53:06 +0300

    Lab3

--- app/src/main/java/com/example/lab3_sharedpreferences/MainActivity.java
+++ app/src/main/java/com/example/lab3_sharedpreferences/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        
 
         dateButton.setOnClickListener(new View.OnClickListener() {
             @Override
